d73e5d74b9d1d4b3d7abae7dc1794bfe68bfbb3d
Niels Swimberghe <3382717+Swimburger@users.noreply.github.com>
2022-01-21 00:37:19 -0500

    Update to .NET6 using minimal APIs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,66 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using System.IO.Compression;
 
-namespace WebZipIt
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages(options =>
 {
-    public class Program
+    options.Conventions.AddFolderRouteModelConvention("/", pageRouteModel =>
     {
-        public static void Main(string[] args)
+        foreach (var selectorModel in pageRouteModel.Selectors)
         {
-            CreateHostBuilder(args).Build().Run();
+            selectorModel.AttributeRouteModel.Template = "pages/" + selectorModel.AttributeRouteModel.Template;
         }
+    });
+});
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/mvc/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapGet("/", async context =>
+{
+    await context.Response.WriteAsync("Request /download-bots to download a zip file of .NET Bots");
+});
+
+app.MapGet("/download-bots", async context =>
+{
+    context.Response.ContentType = "application/octet-stream";
+    context.Response.Headers.Add("Content-Disposition", "attachment; filename=\"Bots.zip\"");
+
+    var botsFolderPath = Path.Combine(app.Environment.ContentRootPath, "bots");
+    var botFilePaths = Directory.GetFiles(botsFolderPath);
+    using (ZipArchive archive = new ZipArchive(context.Response.BodyWriter.AsStream(), ZipArchiveMode.Create))
+    {
+        foreach (var botFilePath in botFilePaths)
+        {
+            var botFileName = Path.GetFileName(botFilePath);
+            var entry = archive.CreateEntry(botFileName);
+            using (var entryStream = entry.Open())
+            using (var fileStream = System.IO.File.OpenRead(botFilePath))
+            {
+                await fileStream.CopyToAsync(entryStream);
+            }
+        }
+    }
+});
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "mvc/{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
